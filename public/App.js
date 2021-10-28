13413dc3cb07760cdff6c2b96f52ddb427c9311d
Tianyu <1374736640@qq.com>
2021-10-28 22:59:42 +0800

    Feat(all): compatible for old browsers

--- public/App.js
+++ public/App.js
@@ -1,4 +1,16 @@
-const element = /*#__PURE__*/React.createElement("div", {
-  title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World"));
+"use strict";
+
+// const element = (
+//   <div title="Outer div">
+//     <h1>Hello World</h1>
+//   </div>
+// );
+//
+// ReactDOM.render(element, document.getElementById('contents'))
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var helloContinents = Array.from(continents, continent => "Hello ".concat(continent, "!"));
+var message = helloContinents.join(' ');
+var element = /*#__PURE__*/React.createElement("div", {
+  title: "outer div"
+}, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+// const element = (
+//   <div title="Outer div">
+//     <h1>Hello World</h1>
+//   </div>
+// );
+//
+// ReactDOM.render(element, document.getElementById('contents'))
+
+const continents = [
+  'Africa', 'America', 'Asia', 'Australia', 'Europe'
+]
+
+const helloContinents = Array.from(continents, continent => `Hello ${continent}!`)
+const message = helloContinents.join(' ')
+
 const element = (
-  <div title="Outer div">
-    <h1>Hello World</h1>
+  <div title="outer div">
+    <h1>{message}</h1>
   </div>
-);
+)
 
 ReactDOM.render(element, document.getElementById('contents'))
