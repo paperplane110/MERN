3afda3f97d5af754c9698d454991928baf1db86a
Tianyu <1374736640@qq.com>
2021-12-07 22:55:40 +0800

    Feat(ui): init webpack

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,30 +1,6 @@
 /* global React ReactDOM PropTypes */
 
-async function graphQLFetch (query, variables = {}) {
-  try {
-    const response = await fetch(window.ENV.UI_API_ENDPOINT, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query, variables })
-    })
-    const body = await response.text()
-    const result = JSON.parse(body, jsonDateReviver)
-
-    if (result.errors) {
-      const error = result.errors[0]
-      if (error.extensions.code === 'BAD_USER_INPUT') {
-        const details = error.extensions.exception.errors.join('\n ')
-        alert(`${error.message}:\n ${details}`)
-      } else {
-        alert(`${error.extensions.code}: ${error.message}`)
-      }
-    }
-    return result.data
-  } catch (e) {
-    alert(`Error in sending data to server: ${e.message}`)
-  }
-}
-
+import graphQLFetch from './graphQLFetch'
 class IssueFilter extends React.Component {
   render () {
     return (
@@ -105,13 +81,6 @@ class IssueAdd extends React.Component {
 
 IssueAdd.propTypes = {
   createIssue: PropTypes.func.isRequired
-}
-
-const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d')
-
-function jsonDateReviver (key, value) {
-  if (dateRegex.test(value)) return new Date(value)
-  return value
 }
 
 class IssueList extends React.Component {
